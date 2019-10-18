b68b6ed460b116c03bd9170fd778d0ec08e01f08
Robert Lubos <robert.lubos@nordicsemi.no>
2019-10-18 12:20:06 +0200

    Bump version to 2.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='piccata',
-      version='2.0.0',
+      version='2.0.1',
       description='Python CoAP Toolkit',
       author='Nordic Semiconductor',
       url='https://github.com/NordicSemiconductor/piccata',
